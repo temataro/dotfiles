0bc5f6e1fd2689b1a40a2c1665032be9867aed50
Temesgen Ataro <temesgen.ataro@tii.ae>
2024-04-14 13:01:46 +0400

    bootstrap: Add arandr for managing monitor setups
    Also add `./xrandr/` directory with a preset for home setup.

---
+++ xrandr/3-monitor-home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1600 --pos 4920x560 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-1-0 --mode 1920x1080 --pos 0x209 --rotate left --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-0 --primary --mode 3840x2160 --pos 1080x0 --rotate normal
