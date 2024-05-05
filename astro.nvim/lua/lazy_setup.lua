77cec827990434a872499807b27cc12e3db217f5
Temesgen Ataro <atarotemesgen@gmail.com>
2024-05-05 10:00:48 +0400

    nvim: Add Astrovim base configuration
    Transparent gruvbox from `ellisonleao/gruvbox.nvim`.

--- astro.nvim/lua/lazy_setup.lua
+++ astro.nvim/lua/lazy_setup.lua
@@ -15,7 +15,8 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax", } },
+  -- install = { colorscheme = { "astrodark", "habamax", } },
+  install = { colorscheme = { 'gruvbox' } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
