b70bd30478da2f64b9eb5f8629c5616471219078
Temesgen Ataro <ataro.temesgen@gmail.com>
2025-04-23 11:26:10 +0400

    lews_therin: Fix formatting and run from cat not glow

--- extra/lews-therin/humming.py
+++ extra/lews-therin/humming.py
@@ -16,28 +16,31 @@ import random
 import subprocess as sp
 
 HIT_PROBABILITY = 85  # probability of showing quote in percents
-QUOTE_DIR = "/home/tem/code/github.com/temataro/dotfiles/extra/lews-therin/all-quotes.txt"
+QUOTE_DIR = (
+    "/home/tem/code/github.com/temataro/dotfiles/extra/lews-therin/all-quotes.txt"
+)
 # All quotes generously taken from the repo where r/lews-therin-bot stores them
 # https://github.com/Tobuss/Lews-Therin-Bot/blob/master/lews.txt
 
 
 def main():
     # Read quotes, pick a random one
-    with open(QUOTE_DIR, 'r') as f:
+    with open(QUOTE_DIR, "r") as f:
         lines = f.readlines()
 
     # Quick test to see if you have glow on your system
-    err = sp.run("which glow".split(), stdout=sp.DEVNULL,
-                 stderr=sp.DEVNULL).returncode
+    err = sp.run("which glow".split(), stdout=sp.DEVNULL, stderr=sp.DEVNULL).returncode
     if err:
-        print("You don't currently have glow on your system. Install from your"
-              "favorite package manager.\n")
+        print(
+            "You don't currently have glow on your system. Install from your"
+            "favorite package manager.\n"
+        )
 
     if random.randint(0, 100) < HIT_PROBABILITY:
-        with open("/tmp/tmp-quote.md", "w") as f:
+        with open("/tmp/tmp-quote.md", 'w') as f:
             f.write(random.choice(lines))
 
-        sp.run("glow /tmp/tmp-quote.md".split())
+        sp.run("cat /tmp/tmp-quote.md".split())
 
 
 if __name__ == "__main__":
