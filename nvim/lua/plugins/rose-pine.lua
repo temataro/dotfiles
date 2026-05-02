5e5b38913ad78d14ed37f0f25d4c3859eaae35a9
Temesgen Ataro <ataro.temesgen@gmail.com>
2026-05-03 02:25:52 +0400

    nvim: Revamped setup. Back to basics.
    Only kept themes, old `.vimrc` settings/keymaps, a file explorer, and a nice dashboard

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -1,7 +1,13 @@
 return {
   "rose-pine/neovim",
   name = "rose-pine",
+  lazy = false,
+  priority = 1000,
   opts = {
     styles = { transparency = true },
   },
+  config = function(_, opts)
+    require("rose-pine").setup(opts)
+    vim.cmd.colorscheme "rose-pine-main"
+  end,
 }
